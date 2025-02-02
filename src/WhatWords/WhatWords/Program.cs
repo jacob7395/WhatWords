84128ab32dbe5bcc5fc4d9b09b8352d2851b3eeb
Jake <jacob7395@gmail.com>
2025-02-02 10:30:23 +0000

    working program to getnerate short common strings

--- src/WhatWords/WhatWords/Program.cs
+++ src/WhatWords/WhatWords/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Immutable;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Text.RegularExpressions;
 using MoreLinq;
-using Spectre.Console;
 using Spectre.Console.Cli;
 using what_word;
 
@@ -20,18 +18,20 @@ internal sealed class CommonLettersCommand : AsyncCommand<CommonLettersCommand.S
         public string IncludedCharacters { get; set; }
 
         [CommandOption("-c|--generate-count <COUNT>")]
-        public int GenerateCount { get; init; } = 100;
+        public int GenerateCount { get; init; } = 200;
 
         [CommandOption("-n|--character-count <COUNT>")]
         public int CharacterCount { get; init; } = 3;
 
-        [CommandArgument(1, "[focuse character]")]
-        public char FocusCharacters { get; init; }
+        [CommandArgument(1, "[focus character]")]
+        public string FocusCharacters { get; init; }
     }
 
     public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
-        var erWordlist = await WordList.Load("common_words.txt");
+        Console.WriteLine("Running");
+
+        var erWordlist = await WordList.Load("frequent_words.txt");
 
         if (erWordlist.IsError || erWordlist.Value is not {} wordList)
         {
@@ -44,22 +44,55 @@ internal sealed class CommonLettersCommand : AsyncCommand<CommonLettersCommand.S
                                   .Select(s => s[0])
                                   .ToImmutableArray();
 
-        var analysis = CharacterAnalysis.GetFrequency(
-            settings.FocusCharacters,
-            wordList,
-            characterArray,
-            settings.CharacterCount).ToImmutableArray();
+        Dictionary<char, ImmutableArray<string>> words = new();
 
-        if (analysis.Length == 0) return 1;
-
-        while (analysis.Length < settings.GenerateCount)
+        foreach (char character in characterArray)
         {
-            analysis = analysis.AddRange(analysis);
+            var analysis = CharacterAnalysis.GetFrequency(
+                                                character,
+                                                wordList,
+                                                characterArray,
+                                                settings.CharacterCount)
+                                            .ToImmutableArray();
+
+            words.Add(character, analysis);
         }
+
+        List<string> output = new();
+        Dictionary<char, int> counters = new();
+
+        characterArray = [..settings.FocusCharacters.Split(' ').Select(s => s[0])];
         
+        while (output.Count < settings.GenerateCount)
+            foreach (char c in characterArray)
+            {
+                if (output.Count >= settings.GenerateCount)
+                {
+                    break;
+                }
+
+                var index = counters.GetValueOrDefault(c);
+                var characterWordList = words.GetValueOrDefault(c);
+
+                if (characterWordList.IsEmpty)
+                {
+                    continue;
+                }
+
+                if (characterWordList.Length <= index)
+                {
+                    index = 0;
+                }
+                
+                output.Add(characterWordList[index++]);
+                counters[c] = index;
+            }
+
         var outputStringBuilder = new StringBuilder();
 
-        outputStringBuilder.AppendJoin(' ', analysis[..settings.GenerateCount].Shuffle());
+        outputStringBuilder.AppendJoin(' ', output.Shuffle());
+
+        Console.Write(outputStringBuilder.ToString());
 
         await File.WriteAllTextAsync("output.txt", outputStringBuilder.ToString());
 
